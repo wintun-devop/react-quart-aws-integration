25a7bf3cf217a85074f25f4b05e78d3da95e42c1
Win Tun <wintun.hlaing@outlook.com>
2024-01-13 20:36:17 +0630

    basic login register dashboard config

---
+++ react-front/src/screens/auth/login/login.tsx
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material";
+
+
+
+const Login = () =>{
+    return(
+        <>
+            <Typography>Login Page</Typography>
+        </>
+    )
+
+}
+
+export default Login;
---
+++ react-front/src/screens/auth/register/register.tsx
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material";
+
+
+
+const Register = () =>{
+    return(
+        <>
+            <Typography>Register Page</Typography>
+        </>
+    )
+
+}
+
+export default Register;
